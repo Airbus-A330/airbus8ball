3c9e586bea7d91ebe9e4a0be927421f371199d93
VietnamAirlines-lab <53490656+VietnamAirlines-lab@users.noreply.github.com>
2019-11-04 20:27:52 -0800

    Update cli.js
    Fixed old names to new names and improved the wrapper and any outputs it gave

--- cli.js
+++ cli.js
@@ -5,10 +5,10 @@ const stdin = process.stdin
 stdin.resume() & stdin.setEncoding('utf8')
 
 const arg = process.argv[2]
-const eightball = require('./')()
-const ask = () => output('please ask a question')
+const airbus8ball = require('./')()
+const ask = () => output('Please ask a question')
 const output = (str) => process.stdout.write(str + '\n') && process.exit()
-const check = (str) => (str && str.match(/\?\s*$/)) ? output(eightball) : ask()
+const check = (str) => (str && str.match(/\?\s*$/)) ? output(airbus8ball) : ask()
 
 if (stdin.isTTY) check(arg)
 
